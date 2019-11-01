c49b70426c5cda25fb5475d3a0515f35363d1878
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-11-01 03:26:14 -0600

    add empty test

---
+++ test/tst_bacio.F90
@@ -0,0 +1,17 @@
+! This program provides an elementary check of some of the parts of the 
+! NCEP bacio library.
+!
+! Ed Hartnett, 1/11/19
+!
+program tst_bacio
+  use bacio_module
+  implicit none
+  
+
+  print *, ''
+  print *,'*** Testing bacio.'
+  
+
+  print *,'*** SUCCESS!'
+
+end program tst_bacio
