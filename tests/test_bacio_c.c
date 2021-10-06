1548e1b99c809738d2b1d9a03caea740c68e284c
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-06 04:37:22 -0600

    adding C test of bacio

--- tests/test_bacio_c.c
+++ tests/test_bacio_c.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdio.h>
+#include <clib.h>
 
 int
 main()
@@ -11,6 +12,19 @@ main()
     printf("Testing NCEPLIBS-bacio.\n");
     printf("Testing bacio call...");
     {
+        int mode = BAOPEN_WONLY;
+        int start = 0;
+        int newpos = 0, size = 4, no = 1, nactual = 0, fdes = 0;
+        const char fname[] = "test_bacio_c.bin";
+        char datary[] = "test";
+        int  namelen, datanamelen;
+        int ierr;
+
+        namelen = strlen(fname);
+        datanamelen = strlen(datary);
+
+        ierr = bacio_(&mode, &start, &newpos, &size, &no, &nactual,
+                      &fdes, fname, datary, namelen, datanamelen);
     }
     printf("ok!\n");
     printf("SUCCESS!\n");
