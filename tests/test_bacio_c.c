e22884b530a0753eeaa30ae26e51e015f0d4887c
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-06 05:07:18 -0600

    now write and read some data with bacio in test

--- tests/test_bacio_c.c
+++ tests/test_bacio_c.c
@@ -11,34 +11,68 @@ int bacio_(int *mode, int *start, int *newpos, int *size, int *no,
            int *nactual, int *fdes, const char *fname, char *datary,
            int  namelen, int  datanamelen);
 
+#define ERR 99
+
 int
 main()
 {
     printf("Testing NCEPLIBS-bacio.\n");
-    printf("Testing bacio call...");
+    printf("Testing some simple bacio_() calls...");
     {
-        int mode = BAOPEN_WONLY;
+        int mode;
         int start = 0;
-        int newpos = 0, size = 4, no = 1, nactual = 0, fdes = 0;
+        int newpos = 0, size = 4, no = 4, nactual, fdes;
         const char fname[] = "test_bacio_c.bin";
         char datary[] = "test";
+        char datary_in[4];
         int  namelen, datanamelen;
+        int i;
         int ierr;
 
         namelen = strlen(fname);
         datanamelen = strlen(datary);
 
         /* Create the file. */
+        mode = BAOPEN_WONLY;
         if ((ierr = bacio_(&mode, &start, &newpos, &size, &no, &nactual,
                            &fdes, fname, datary, namelen, datanamelen)))
             return ierr;
+
+        /* Write some data. */
+        mode = BAWRITE;
+        if ((ierr = bacio_(&mode, &start, &newpos, &size, &no, &nactual,
+                           &fdes, fname, datary, namelen, datanamelen)))
+            return ierr;
+        if (nactual != no) return ERR;
+
 
         /* Close the file. */
         mode = BACLOSE;
         if ((ierr = bacio_(&mode, &start, &newpos, &size, &no, &nactual,
                            &fdes, fname, datary, namelen, datanamelen)))
             return ierr;
-        
+
+        /* Reopen the file. */
+        mode = BAOPEN_RONLY;
+        if ((ierr = bacio_(&mode, &start, &newpos, &size, &no, &nactual,
+                           &fdes, fname, datary, namelen, datanamelen)))
+            return ierr;
+
+        /* Read the data we just wrote. */
+        mode = BAREAD;
+        if ((ierr = bacio_(&mode, &start, &newpos, &size, &no, &nactual,
+                           &fdes, fname, datary_in, namelen, datanamelen)))
+            return ierr;
+        if (nactual != no) return ERR;
+        for (int i = 0; i < 4; i++)
+            if (datary[i] != datary_in[i]) return ERR;
+
+        /* Close the file. */
+        mode = BACLOSE;
+        if ((ierr = bacio_(&mode, &start, &newpos, &size, &no, &nactual,
+                           &fdes, fname, datary, namelen, datanamelen)))
+            return ierr;
+
     }
     printf("ok!\n");
     printf("SUCCESS!\n");
