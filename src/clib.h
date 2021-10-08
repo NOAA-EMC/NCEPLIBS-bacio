f035a58bd5d0e0b510497a9da3d6727d5275b473
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-08 07:27:42 -0600

    improved documentation

--- src/clib.h
+++ src/clib.h
@@ -19,12 +19,12 @@
 #include <stdlib.h>
 
 /* IO-related (bacio.c, banio.c). */
-#define BAOPEN_RONLY              1 /**< Read only. */
-#define BAOPEN_WONLY              2 /**< Write only. */
-#define BAOPEN_RW                 4 /**< Read/write. */
-#define BACLOSE                   8 /**< Close. */
-#define BAREAD                   16 /**< Read. */
-#define BAWRITE                  32 /**< Write. */
-#define NOSEEK                   64 /**< No seek. */
-#define BAOPEN_WONLY_TRUNC      128 /**< Write only truncate. */
-#define BAOPEN_WONLY_APPEND     256 /**< Write only append. */
+#define BAOPEN_RONLY              1 /**< Open file read only. */
+#define BAOPEN_WONLY              2 /**< Open or create file for Write only. */
+#define BAOPEN_RW                 4 /**< Open or create file for read/write. */
+#define BACLOSE                   8 /**< Close an open file. */
+#define BAREAD                   16 /**< Read from an open file. */
+#define BAWRITE                  32 /**< Write to an open file. */
+#define NOSEEK                   64 /**< No seek ignore start parameter and do not call lseek(). */
+#define BAOPEN_WONLY_TRUNC      128 /**< Open or create a file for write only, truncating existing contents. */
+#define BAOPEN_WONLY_APPEND     256 /**< Open or create a file for write only append. */
