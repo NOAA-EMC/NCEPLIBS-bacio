4fc140b0e17fbb4cb58eb71e6c193339fd0997f5
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-08 07:03:06 -0600

    more fortran testing

--- tests/test_bacio2.f90
+++ tests/test_bacio2.f90
@@ -19,7 +19,7 @@ program test_bacio2
 
   print *, 'Testing bacio.'
   
-  print *, 'Testing simple wryte()/wrytel() calls - error messages are expected...'
+  print *, 'Testing simple wryte() calls - error messages are expected...'
 
   ! Delete the test file, if it remains from previous runs.
   open(unit = 1234, iostat = stat, file = filename, status='old')
@@ -37,6 +37,37 @@ program test_bacio2
 
   ! Write some data.
   call wrytel(lu, 4_8, data)
+
+  ! Close the test file.
+  call baclose(lu, iret)
+  if (iret .ne. 0) stop 12
+
+  ! Reopen the test file.
+  call baopenr(lu, filename, iret)
+  if (iret .ne. 0) stop 22
+
+  ! Read some data.
+  call baread(lu, 0, 4, ka, data_in)
+  if (ka .ne. 4) stop 31
+  if (data_in .ne. data) stop 32
+
+  ! Close the test file.
+  call baclose(lu, iret)
+  if (iret .ne. 0) stop 40
+
+  print *, 'Testing simple wrytel() calls - error messages are expected...'
+
+  data = 'bill'
+  
+  ! Create a test file.
+  call baopenwt(lu, filename, iret)
+  if (iret .ne. 0) stop 3
+
+  ! Try to write some data - won't work, does nothing due to negative size.
+  call wryte(lu, -4, data)
+
+  ! Write some data.
+  call wryte(lu, 4, data)
 
   ! Close the test file.
   call baclose(lu, iret)
