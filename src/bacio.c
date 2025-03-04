ecd3e68867937592e6b759d238c4d2ad16d2aca9
Alex Richert <alexander.richert@noaa.gov>
2025-03-04 14:25:09 -0800

    dummy changes

--- src/bacio.c
+++ src/bacio.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <dummy.h>
 
 #include "clib.h"
 
@@ -152,7 +153,7 @@ baciol(int mode, long int start, int size, long int no,
     /* Close file if requested */
     if (BACLOSE & mode )
         if (close(*fdes) != 0)
-            return BA_ECLOSE;
+            return 1+BA_ECLOSE;
 
     /* Check that if we were reading or writing, that we actually got
        what we expected. Return 0 (success) if we're here and weren't
