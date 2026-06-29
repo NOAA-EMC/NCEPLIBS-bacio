bbc3d275eb712ee041b4e3dd1b1432f0aaffa891
Hang-Lei-NOAA <44901908+Hang-Lei-NOAA@users.noreply.github.com>
2026-06-29 10:24:48 -0400

    Refactor test_byteswap.c to fix developer CI test
    Removed redundant variable declarations for int_data and ret when ENABLE_DEPRECATED_SUBS is defined.

--- tests/test_byteswap.c
+++ tests/test_byteswap.c
@@ -24,9 +24,11 @@ main()
     {
         unsigned char byte_data = 42;
         short int short_data = 42;
+        int ret;
+#ifdef ENABLE_DEPRECATED_SUBS
         int int_data = 42;
         long long int int64_data = 42;
-        int ret;
+#endif
         
         /* Turn off error messages. */
         fast_byteswap_errors(0);
@@ -36,9 +38,6 @@ main()
             return ERR;
 
 #ifdef ENABLE_DEPRECATED_SUBS
-        int int_data = 42;
-        long long int int64_data = 42;
-        int ret;
         /* Swap a short. */
         if ((ret = fast_byteswap(&short_data, 2, 1)) != 1)
             return ERR;
